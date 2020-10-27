d6d416a8eba4799c4ea8160a3dad27b843204b61
Tay Yang Wong <wongtakkyan@gmail.com>
2020-10-27 21:56:57 +0100

    MVP version pushing to Github

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: %i[index show new create] do
+  resources :cocktails, only: %i[index show new create destroy] do
     resources :doses, only: %i[new create]
   end
   delete '/doses/:id', to: 'doses#destroy', as: 'dose'
